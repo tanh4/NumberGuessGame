9cf831f775dd368d8124e5db01c673c876bdfa10
xiaohang wang <derekwang@xiaohangs-MacBook-Pro.local>
2019-05-08 00:04:51 -0700

    add type check for invalid inout

--- NumberGuessGame.py
+++ NumberGuessGame.py
@@ -1,23 +1,40 @@
 from random import randint
 
+def IsInt(str):
+    try: 
+        int(str)
+        return True
+    except ValueError:
+        return False
+
 target = randint(1, 100)
 
-currentInput = int(input("Please input a number between 1 - 100:"))
+currentInput = "initial value"
+while not IsInt(currentInput):
+    currentInput = input("Please input 1 - 100! Or input Q to quit.")
+    if currentInput == 'Q':
+        quit()
+lastDiff = abs(int(currentInput) - target)
+
+currentInput = int(currentInput)
 if currentInput < 1 or currentInput > 100:
     print("OUT OF BOUNDS!")
-
-lastDiff = abs(currentInput - target)
-if lastDiff <= 10:
-    print("Warm!")
-elif lastDiff == 0:
-    print("Win!")
 else:
-    print("Cold!")
+    lastDiff = abs(currentInput - target)
+    if lastDiff <= 10:
+      print("Warm!")
+    elif lastDiff == 0:
+        print("Win!")
+    else:
+        print("Cold!")
 
 while input != target:
-    currentInput = input("Press Q to quit, otherwise input a guessed number:")
+    currentInput = input("Press Q to quit, otherwise input a guessed number between 1 - 100:")
     if currentInput == 'Q':
         break
+    if not IsInt(currentInput):
+        continue
+        
     currentInput = int(currentInput)
     diff = abs(currentInput - target)
     if currentInput < 1 or currentInput > 100:
